758e55db11c1b37c5ce95b5e625fc48e83ad6824
Jakub Bednarz <jakubbednarz31@gmail.com>
2025-10-15 22:21:20 +0200

    Created main file and basic game engine

---
+++ Game.py
@@ -0,0 +1,32 @@
+import pygame
+WIDTH = 1024
+HEIGHT = 1024
+FPS = 60
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Zombie Survival Prototype")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(FPS) / 1000.0
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.running = False
+                if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                    self.running = False    
+            self.update(dt)
+            self.draw()
+        pygame.quit()
+    
+    # =============== TODO =============
+    def update(self, dt: float):
+        print(123) 
+
+    # =============== TODO =============
+    def draw(self):
+        print(123)
--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-import objects
-import pygame
+from Game import Game
 
-# --- Settings ---
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-FPS = 60
-
-# --- Initialize Pygame ---
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("AiBasics")
-clock = pygame.time.Clock()
-
-# --- Main Game Loop ---
-running = True
-while running:
-    screen.fill((20, 20, 30))  # Dark background
-
-    # Event Handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Draw Obstacles
-    for polygon in objects.obstacles:
-        pygame.draw.polygon(screen, (60, 180, 180), polygon)
-        pygame.draw.polygon(screen, (255, 255, 255), polygon, 2)  # outline
-
-    pygame.display.flip()
-    clock.tick(FPS)
-
-pygame.quit()
+if __name__ == '__main__':
+    Game().run()
